ff8e8a2c7233b52fcb86cfda21faa30ef2ea93c1
kingclassicman <kingclassicman@gmail.com>
2026-06-03 11:32:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ elif st.button("💰 ทำความสะอาดข้อมูล"):
 elif st.button("💰 ทำความสะอาดข้อมูล Customer"):
     st.switch_page("pages/clean_customers.py")
 elif st.button("💰 แปลงข้อมูล"):
-    st.switch_page("pages/transform.app.py")
+    st.switch_page("pages/transform_app.py")
 
 
